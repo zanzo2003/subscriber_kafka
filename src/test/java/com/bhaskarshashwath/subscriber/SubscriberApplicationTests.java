82214ee02c67848570ccbb9920602804ea919091
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-06-14 18:42:06 +0530

    setup: setting up demo applicaiton for kafka subscriber

---
+++ src/test/java/com/bhaskarshashwath/subscriber/SubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskarshashwath.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
